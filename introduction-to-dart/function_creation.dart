e2d2c133df12e4d4971a9802a6f964e2bf62a754
Josue Alonzo-Chavarria <joshi.alonzo@gmail.com>
2023-05-18 21:45:43 -0600

    Dart: Create a function

---
+++ introduction-to-dart/function_creation.dart
@@ -0,0 +1,9 @@
+void main() {
+    double calculateCircleArea(double radius) {
+        // code to calculate the area goes here
+        double area = 3.14 * radius * radius;
+        return area;
+    }
+
+    print(calculateCircleArea(5));
+}
