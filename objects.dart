19e9f5a9a0c54d42c76fb260c103e5520404aba6
Josue Alonzo-Chavarria <joshi.alonzo@gmail.com>
2023-05-18 19:17:54 -0600

    Dart: Objects

---
+++ objects.dart
@@ -0,0 +1,7 @@
+/*
+Objects:
+    Characteristics:
+        - Identity: Unique object identifier
+        - State: Representation of objects properties at a given time
+        - Behavior: What an object can do or actions it can take
+*/
